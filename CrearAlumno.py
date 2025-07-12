f1afdc1544bdb086b5196fdf0df5d7209a136947
salaverryyy <166071559+salaverryyy@users.noreply.github.com>
2025-07-11 23:59:04 -0500

    Update CrearAlumno.py

--- CrearAlumno.py
+++ CrearAlumno.py
@@ -5,7 +5,7 @@ def lambda_handler(event, context):
 # Publicar en SNS
 sns_client = boto3.client('sns')
 response_sns = sns_client.publish(
-    TopicArn = 'arn:aws:sns:us-east-1:447891120606:NuevoAlumno',
+    TopicArn = 'arn:aws:sns:us-east-1:570699102669:NuevoAlumno',
     Subject = 'Nuevo Alumno',
     Message = json.dumps(alumno),
     MessageAttributes = {
