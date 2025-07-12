b254d496b6bebc3072fdc91becb291cd72f144f9
salaverryyy <166071559+salaverryyy@users.noreply.github.com>
2025-07-11 23:53:47 -0500

    Add files via upload

---
+++ CrearAlumno.py
@@ -0,0 +1,20 @@
+import json
+import boto3
+def lambda_handler(event, context):
+
+# Publicar en SNS
+sns_client = boto3.client('sns')
+response_sns = sns_client.publish(
+    TopicArn = 'arn:aws:sns:us-east-1:447891120606:NuevoAlumno',
+    Subject = 'Nuevo Alumno',
+    Message = json.dumps(alumno),
+    MessageAttributes = {
+        'tenant_id': {'DataType': 'String', 'StringValue': tenant_id }
+    }
+)
+print(response_sns)
+# Salida (json)
+return {
+    'statusCode': 200,
+    'response': response
+}
